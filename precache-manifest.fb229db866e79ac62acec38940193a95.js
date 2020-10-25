48ad6c9ae2f8a3e3b8e4938d4c5b1e349368bb02
bru2313 <bru@adaptiveresponse.net>
2020-10-25 15:30:51 +0000

    Updates

---
+++ precache-manifest.fb229db866e79ac62acec38940193a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a590bd3fd53b4477038c2924062ac5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bdb52ac1d99c8138a939",
+    "url": "/monsters-rolodex/static/css/main.9e24f49e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdb52ac1d99c8138a939",
+    "url": "/monsters-rolodex/static/js/main.943176b8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
